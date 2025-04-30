0530d1c2173dad5ae1607ab3f1b24b30419eca84
schrammelb <bjorn@schrammel.dev>
2025-04-30 13:33:48 +0200

    Add authentication and client classes to module exports

--- api_essentials/__init__.py
+++ api_essentials/__init__.py
@@ -1,6 +1,32 @@
 from api_essentials.factory import APIFactory
+from api_essentials.auth import OAuth2Auth, ClientCredentials, UserCredentials, TokenCredentials, ApiCredentials
+from api_essentials.client import APIClient
+from api_essentials.endpoint import Endpoint
+from api_essentials.parameter import ParameterFactoryService as ParameterFactory
+from api_essentials.flags import (
+    USE_DEFAULT_POST_RESPONSE_HOOK,
+    ALLOW_UNSECURE,
+    FORCE_HTTPS,
+    TRUST_UNDEFINED_PARAMETERS
+)
+
 
 
 __all__ = [
-    "APIFactory"
+    "OAuth2Auth",
+
+    "ClientCredentials",
+    "UserCredentials",
+    "TokenCredentials",
+    "ApiCredentials",
+
+    "APIClient",
+    "Endpoint",
+    "ParameterFactory",
+    "APIFactory",
+
+    "USE_DEFAULT_POST_RESPONSE_HOOK",
+    "ALLOW_UNSECURE",
+    "FORCE_HTTPS",
+    "TRUST_UNDEFINED_PARAMETERS"
 ]
