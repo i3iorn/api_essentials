e75441f094996c2251a2980fb53d78166fd538df
schrammelb <bjorn@schrammel.dev>
2025-05-13 20:00:20 +0200

    Rename OAuth2Auth to OAuth2Flow in __init__.py

--- api_essentials/auth/__init__.py
+++ api_essentials/auth/__init__.py
@@ -1,10 +1,10 @@
-from api_essentials.auth.flow import OAuth2Auth, TokenAuth
+from api_essentials.auth.flow import OAuth2Flow, TokenAuth
 from api_essentials.auth.info import AbstractCredentials, ClientCredentials, UserCredentials, TokenCredentials, ApiCredentials
 
 
 __all__ = [
     "TokenAuth",
-    "OAuth2Auth",
+    "OAuth2Flow",
     "AbstractCredentials",
     "ClientCredentials",
     "UserCredentials",
