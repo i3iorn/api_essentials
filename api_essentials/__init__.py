839173e9f1cc75df75787413a980743bb4b8ecf6
schrammelb <bjorn@schrammel.dev>
2025-06-03 23:08:08 +0200

    Update __init__.py to include BaseAuth in imports and enhance module exports

--- api_essentials/__init__.py
+++ api_essentials/__init__.py
@@ -1,4 +1,4 @@
-from api_essentials.auth.oauth2 import BaseOAuth2
+from api_essentials.auth.oauth2 import BaseOAuth2, BaseAuth
 from api_essentials.auth.token import OAuth2Token
 from api_essentials.auth.config import OAuth2Config
 from api_essentials.auth.exceptions import (
@@ -12,3 +12,14 @@ from api_essentials.utils.log import setup_secret_filter
 
 setup_secret_filter()  # Ensure all logs are filtered for secrets
 
+__all__ = [
+    "BaseOAuth2",
+    "BaseAuth",
+    "OAuth2Token",
+    "OAuth2Config",
+    "OAuth2TokenExpired",
+    "OAuth2TokenInvalid",
+    "OAuth2TokenRevoked",
+    "OAuth2TokenException",
+    "OAuth2Exception",
+]
